cb9dab9b65137c29eafb5f1928d9fa3bfa0e8bc3
Teymen <72708603+TDNhd@users.noreply.github.com>
2021-02-18 15:20:54 +0100

    Add files via upload
    Added support for configuration file

--- garage-door-opener.py
+++ garage-door-opener.py
@@ -1,18 +1,33 @@
+from configparser import ConfigParser
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import time
 
-relay = 17          #Optionally set this to desired pin
-reedOpen = 5        #Optionally set this to desired pin
-reedClosed = 12     #Optionally set this to desired pin
+#Reads config.ini file
+config_location = 'home/pi/garage/config.ini'
+config = ConfigParser()
+config.read(config_location)
+
+broker = str(config['mqtt']['brokerAddress'])
+port = (config['mqtt']['port'])     #Port "8883" needed when running SSL/TLS encrypted connection (default TCP/IP port is 1883)
+clientID = (config['mqtt']['client_id'])
+tls_set = (config['tls']['tls_set'])
+if tls_set:
+    CA_crt = str(config['tls']['CA_crt'])
+    Client_crt = str(config['tls']['Client_crt'])
+    Client_key = str(config['tls']['Client_key'])
+    tls_insecure_set = (config['tls']['tls_insecure_set'])
+else:
+    CA_crt = Client_crt = Client_key = tls_insecure_set = None
+relay = (config['GPIO']['relay'])
+reedOpen = (config['GPIO']['reedOpen'])
+reedClosed = (config['GPIO']['reedClosed'])
+
+#GPIO setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(reedOpen, GPIO.IN)
 GPIO.setup(reedClosed, GPIO.IN)
 GPIO.setup(relay, GPIO.OUT)
-
-broker = "<your Broker address>"
-tls_port = 8883     #Port "8883" needed when running SSL/TLS encrypted connection (default TCP/IP port is 1883)
-client = mqtt.Client(client_id="garage", clean_session=False)
 
 status = ""
 last = ""
@@ -52,11 +67,13 @@ def on_message(client, userdata, message):
         client.publish(topic="garage/getTargetDoorState", payload="close", qos=1, retain=True)
         relay_on_off()
 
-#Connects to mqtt Broker
+#Connects to mqtt Broker    
+client = mqtt.Client(client_id=clientID, clean_session=False)
 client.on_message=on_message
-client.tls_set(ca_certs="<CA certificate location path>", certfile="<Client certificate location path>", keyfile="<Client key location path>")     #Only needed when running SSL/TLS encrypted connection
-client.tls_insecure_set(True)       #Needed when certificate is self-signed
-client.connect(host=broker, port=tls_port)
+if tls_set:
+    client.tls_set(ca_certs=CA_crt, certfile=Client_crt, keyfile=Client_key)     #Only needed when running SSL/TLS encrypted connection
+    client.tls_insecure_set(tls_insecure_set)       #Needed when certificate is self-signed
+client.connect(host=broker, port=port)
 client.subscribe(topic="garage/setTargetDoorState", qos=1)
 client.loop_start()
 
